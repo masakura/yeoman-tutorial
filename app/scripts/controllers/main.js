1d32cd94df4c9d560561002b9197647319c9b826
Tomo Masakura <tomo.masakura@gmail.com>
2015-02-25 11:12:06 +0900

    Model を Model らしく!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var TodoMediator = function (todos) {
+  this.todos = todos || [];
+  this.todo = '';
+};
+
+TodoMediator.prototype.submit = function () {
+  this.todos.push(this.todo);
+  this.todo = '';
+};
+
+TodoMediator.prototype.removeAt = function (index) {
+  this.todos.splice(index, 1);
+};
+
 /**
  * @ngdoc function
  * @name yeomanTutorialApp.controller:MainCtrl
@@ -11,18 +25,15 @@ angular.module('yeomanTutorialApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
     var todosInStore = localStorageService.get('todos');
 
-    $scope.todos = todosInStore || [];
+    var mediator = new TodoMediator(todosInStore);
 
-    $scope.$watch('todos', function () {
-      localStorageService.set('todos', $scope.todos);
+    $scope.mediator = mediator;
+
+    $scope.$watch('mediator.todos', function () {
+      localStorageService.set('todos', mediator.todos);
     }, true);
 
-    $scope.addTodo = function () {
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
-    };
+    $scope.addTodo = function () { mediator.submit(); };
 
-    $scope.removeTodo = function (index) {
-      $scope.todos.splice(index, 1);
-    };
+    $scope.removeTodo = function (index) { mediator.removeAt(index); };
   });
